675394ce7cc00b0580461728f869c0476bcb1530
JuanMa Ruiz <ruizjuanma@gmail.com>
2020-09-07 19:39:41 +0200

    Upgrade generator with "prefix" flag
    * If "prefix" flag is passed the output will contain isbn-10 or isbn-13.
    * Add repository and homepage to package json.
    * Add jest to test configuration in .eslintrc.json

--- src/generator.js
+++ src/generator.js
@@ -4,11 +4,23 @@ const isbnTypes = {
   13: '13'
 };
 
-const generator = (type = isbnTypes[10]) => {
+const isValidString = (value) => typeof value == 'string' || value instanceof String;
+
+const validateOption = (option = '') => {
+  if (isValidString(option) || option === 'prefix') {
+    return Boolean(option);
+  } else {
+    console.error(`The option "${option}" passed is not correct. Pass "prefix" instead."`);
+    process.exit(1);
+  }
+};
+
+const generator = (type = isbnTypes[10], option) => {
+  const prefix = validateOption(option) ? `isbn-${type}: ` : '';
   const randomNumber = parseInt(Math.random() * parseInt(Math.pow(10,type)));
   if ( type === isbnTypes[type] ) {
     const tempIsbn = randomNumber.toString();
-    return validator(tempIsbn) ? `isbn-${type}: ${tempIsbn}` : generator(type);
+    return validator(tempIsbn) ? `${prefix}${tempIsbn}` : generator(type, option);
   } else {
     console.error('Invalid option. Pass "10" for isbn-10 and "13" for isbn-13');
     process.exit(1);
